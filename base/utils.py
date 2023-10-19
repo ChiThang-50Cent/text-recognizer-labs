386c9577357b11b83d435e3b4ffc5664bb7655cd
ChiThang-50Cent <chithang.nydo@gmail.com>
2023-10-20 00:13:43 +0700

    update

--- base/utils.py
+++ base/utils.py
@@ -83,4 +83,4 @@ def training_loop(n_epochs, model, train_loader, val_loader, loss_fn, optimizer,
         total_val_acc += flat_acc(y_hat, y)
 
     print(f'Valid loss {total_val_loss/len(val_loader):.2f}.', end=' ')
-    print(f'Valid acc {total_val_acc/len(val_loader):.2f}.', end=' ')
+    print(f'Valid acc {total_val_acc/len(val_loader):.2f}.')
